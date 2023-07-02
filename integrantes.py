87af49fbc8c4c4108a895d4f1c99fb3a22f2029a
JpDuoc <jua.moram@duocuc.cl>
2023-07-02 00:15:06 +0000

    Script de los integrantes del grupo en Python

---
+++ integrantes.py
@@ -0,0 +1,9 @@
+print("")
+print("Examen Transversal Programación y Redes Virtualizadas – DRY7122.")
+print("")
+
+integrantes = ["1. Juan Pablo Mora", "2. Javier Riquelme", "3. Cristóbal Fuica", "4. Matías Acuña"]
+
+for integrante in integrantes:
+    print(integrante)
+print("")
